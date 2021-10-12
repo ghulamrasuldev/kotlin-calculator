160799c16c23900781bbf922b01b6683702f966c
ghulamrasuldev <ghulam.rasool.development@gmail.com>
2021-10-13 03:33:38 +0500

    creating UI

--- app/src/main/java/com/example/kotlincalculator/MainActivity.kt
+++ app/src/main/java/com/example/kotlincalculator/MainActivity.kt
@@ -3,15 +3,35 @@ package com.example.kotlincalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    var lastNumeric : Boolean = false
+    var lastDot : Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
     fun onDigit(view: View){
-        Toast.makeText(this, "Button Works", Toast.LENGTH_SHORT).show()
+        findViewById<TextView>(R.id.tvInput).append((view as Button).text)
+        lastNumeric = true
+    }
+
+    fun onCLR(view: View){
+        findViewById<TextView>(R.id.tvInput).text = ""
+        lastNumeric = false
+        lastDot = false
+    }
+
+    fun onPoint(view: View){
+        if(lastNumeric && !lastDot){
+            findViewById<TextView>(R.id.tvInput).append((view as Button).text)
+            lastNumeric = false
+            lastDot = true
+        }
     }
 }
