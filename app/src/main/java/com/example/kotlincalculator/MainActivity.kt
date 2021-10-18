fce6b061f8dd8051017bbb4797948639804585d8
ghulamrasuldev <ghulam.rasool.development@gmail.com>
2021-10-18 10:15:33 +0500

    Added Operator Functionalities

--- app/src/main/java/com/example/kotlincalculator/MainActivity.kt
+++ app/src/main/java/com/example/kotlincalculator/MainActivity.kt
@@ -43,6 +43,9 @@ class MainActivity : AppCompatActivity() {
             lastDot = false
             lastNumeric = false
         }
+        if ((view as Button).text =="-" && !findViewById<TextView>(R.id.tvInput).text.toString().endsWith("-")){
+            findViewById<TextView>(R.id.tvInput).append((view as Button).text)
+        }
     }
     private fun isOperatorAdded (value: String): Boolean {
         return if (value.startsWith("-")) {
@@ -52,4 +55,41 @@ class MainActivity : AppCompatActivity() {
             value.contains("+") || value.contains("/") || value.contains("+")
         }
     }
+
+    fun onEqual(view: View){
+        if (lastNumeric){
+            var tvValue = findViewById<TextView>(R.id.tvInput).text.toString()
+            var prefix =""
+            try {
+                if (tvValue.startsWith("-")){
+                    prefix = "-"
+                    tvValue = tvValue.substring(1)
+                }
+                if (tvValue.contains("-")){
+                    val splitValue = tvValue.split("-")
+                    if (prefix == "-"){
+                        findViewById<TextView>(R.id.tvInput).text = ((splitValue[0].toDouble()*(-1))-splitValue[1].toDouble()).toString()
+                    }
+                    else{
+                        findViewById<TextView>(R.id.tvInput).text = (splitValue[0].toDouble()-splitValue[1].toDouble()).toString()
+                    }
+                }
+                else if (tvValue.contains("+")){
+                    val splitValue = tvValue.split("+")
+                    findViewById<TextView>(R.id.tvInput).text = (splitValue[0].toDouble()+splitValue[1].toDouble()).toString()
+                }
+                else if (tvValue.contains("*")){
+                    val splitValue = tvValue.split("*")
+                    findViewById<TextView>(R.id.tvInput).text = (splitValue[0].toDouble()*splitValue[1].toDouble()).toString()
+                }
+                else if (tvValue.contains("/")){
+                    val splitValue = tvValue.split("/")
+                    findViewById<TextView>(R.id.tvInput).text = (splitValue[0].toDouble()/splitValue[1].toDouble()).toString()
+                }
+            }catch (e: ArithmeticException){
+                e.printStackTrace()
+            }
+        }
+    }
+
 }
